cd5ac62e190790d1d84abf569ba4c2cfb1f6cd11
AnSuSh <manjithayas@gmail.com>
2024-10-22 16:53:52 +0530

    Sample project as guided by Philipp.

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -3,21 +3,27 @@ package com.example.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -35,7 +41,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    StateSample(Modifier.fillMaxSize())
+                    LearnSample()
                 }
             }
         }
@@ -43,18 +49,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun StateSample(modifier: Modifier = Modifier) {
+fun LearnSample() {
+    var name by remember {
+        mutableStateOf("")
+    }
+    var names by remember {
+        mutableStateOf(listOf<String>())
+    }
+
     Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.SpaceAround,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        var count by remember {
-            mutableIntStateOf(0)
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(onClick = {
+                names = names + name
+                name = ""
+            }) {
+                Text(text = "Add")
+            }
         }
-        Text(text = "Hero hit me $count times", style = MaterialTheme.typography.displaySmall)
-        Button(onClick = { count++ }, modifier = Modifier.padding(8.dp)) {
-            Text(text = "Click me", style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(names) { addedName ->
+                Text(
+                    text = addedName,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+                HorizontalDivider()
+            }
         }
     }
 }
@@ -65,6 +101,6 @@ fun StateSample(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AndroidPlayGroundTheme {
-        StateSample()
+        LearnSample()
     }
 }
