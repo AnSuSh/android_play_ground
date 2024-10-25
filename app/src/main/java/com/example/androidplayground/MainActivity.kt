6746b4e1cbca7a5c2281ea5538190db300fd4b4c
AnSuSh <manjithayas@gmail.com>
2024-10-25 18:19:19 +0530

    Image card reusable composable.

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -3,28 +3,30 @@ package com.example.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
@@ -41,7 +43,11 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    LearnSample()
+                    ImageCard(
+                        painter = painterResource(id = R.drawable.kemit),
+                        contentDescription = "Kermit the hero",
+                        title = "Kermit in the holidays!"
+                    )
                 }
             }
         }
@@ -49,48 +55,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LearnSample() {
-    var name by remember {
-        mutableStateOf("")
-    }
-    var names by remember {
-        mutableStateOf(listOf<String>())
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .width(180.dp)
+            .height(200.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            OutlinedTextField(
-                value = name,
-                onValueChange = {
-                    name = it
-                },
-                modifier = Modifier.weight(1f)
+        Box {
+            Image(
+                painter = painter, contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
             )
-            Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = {
-                names = names + name
-                name = ""
-            }) {
-                Text(text = "Add")
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        LazyColumn {
-            items(names) { addedName ->
-                Text(
-                    text = addedName,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                )
-                HorizontalDivider()
-            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                MaterialTheme.colorScheme.onSurface
+                            ),
+                            startY = 300f
+                        )
+                    ),
+            )
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.surface,
+                modifier = Modifier
+                    .padding(12.dp)
+                    .align(Alignment.BottomStart)
+            )
         }
     }
 }
@@ -100,7 +103,21 @@ fun LearnSample() {
 @PreviewScreenSizes
 @Composable
 fun GreetingPreview() {
-    AndroidPlayGroundTheme {
-        LearnSample()
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        ImageCard(
+            painter = painterResource(id = R.drawable.kemit),
+            contentDescription = "Kermit the Zero",
+            title = "Kermit in the Woods!"
+        )
+        ImageCard(
+            painter = painterResource(id = R.drawable.kemit),
+            contentDescription = "Kermit the hero",
+            title = "Kermit in the holidays!"
+        )
     }
 }
