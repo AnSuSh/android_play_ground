f22c728f0b5f825e11e168b3774541a1ecdeacac
AnSuSh <manjithayas@gmail.com>
2024-11-08 18:14:16 +0530

    Services Basic step.

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,41 +1,28 @@
 package com.example.androidplayground
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
-import androidx.compose.ui.unit.dp
-import com.example.androidplayground.data.Message
+import androidx.lifecycle.lifecycleScope
 import com.example.androidplayground.data.SampleMessagingData
 import com.example.androidplayground.screens.Conversation
+import com.example.androidplayground.services.SampleService
 import com.example.androidplayground.ui.theme.AndroidPlayGroundTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var intent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +34,21 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        lifecycleScope.launch {
+            delay(2000)
+            Log.d("Main", "Launching the service")
+
+            intent = Intent(this@MainActivity, SampleService::class.java)
+            intent.putExtra("name", "Hero!")
+            startService(intent)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        stopService(intent)
     }
 }
 
